874d1c05f411ecfbd56dfbac5b71c433487ff422
Wellwick <mbbexley@hotmail.co.uk>
2017-01-13 20:42:46 +0000

    Changed gluPerspective so nearer things are visible

--- coaster.cpp
+++ coaster.cpp
@@ -217,8 +217,8 @@ void display()
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     if (g_coasting) {
-	gluLookAt(pos[0][0], pos[0][1]+0.2f, pos[0][2], // eye position
-		  pos[0][0]+dir[0][0], pos[0][1]+0.2f+dir[0][1], pos[0][2]+dir[0][2], // reference point
+	gluLookAt(pos[0][0], pos[0][1], pos[0][2], // eye position
+		  pos[0][0]+dir[0][0], pos[0][1]+dir[0][1], pos[0][2]+dir[0][2], // reference point
 		  0, 1, 0  // up vector
 		);
     } else {
@@ -237,14 +237,74 @@ void display()
 	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
 	
 	glDisable(GL_LIGHTING);
-
-	glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
-	glBegin(GL_QUADS);
-	    glVertex3f(-1.0f, -1.0f, -1.0f);
-	    glVertex3f(-1.0f, -1.0f, 1.0f);
-	    glVertex3f(1.0f, -1.0f, 1.0f);
-	    glVertex3f(1.0f, -1.0f, -1.0f);
-	glEnd();
+	
+	//draw checkerboard surroundings
+	for (int i=0; i<20; i++) {
+	    for (int j=0; j<20; j++) {
+		//draw floor first
+		//greenish floor
+		if ((i+j)%2 == 0)
+		    glColor4f(0.5f, 0.7f, 0.5f, 1.0f);
+		else
+		    glColor4f(0.8f, 1.0f, 0.8f, 1.0f);
+		
+		float x = (float)i / 10;
+		x--;
+		float y = (float)j / 10;
+		y--;
+		
+		//draw floor
+		glBegin(GL_QUADS);
+		    glVertex3f(x, -1.0f, y);
+		    glVertex3f(x, -1.0f, y+0.1f);
+		    glVertex3f(x+0.1f, -1.0f, y+0.1f);
+		    glVertex3f(x+0.1f, -1.0f, y);
+		glEnd();
+		
+		//draw back walls
+		//bluish for sky
+		if ((i+j)%2 == 0)
+		    glColor4f(0.5f, 0.5f, 0.7f, 1.0f);
+		else
+		    glColor4f(0.9f, 0.9f, 1.0f, 1.0f);
+		
+		//draw the walls
+		glBegin(GL_QUADS);
+		    glVertex3f(x, y, -1.0f);
+		    glVertex3f(x, y+0.1f, -1.0f);
+		    glVertex3f(x+0.1f, y+0.1f, -1.0f);
+		    glVertex3f(x+0.1f, y, -1.0f);
+		glEnd();
+		glBegin(GL_QUADS);
+		    glVertex3f(-1.0f, y, x);
+		    glVertex3f(-1.0f, y+0.1f, x);
+		    glVertex3f(-1.0f, y+0.1f, x+0.1f);
+		    glVertex3f(-1.0f, y, x+0.1f);
+		glEnd();
+		
+		if (g_coasting) {
+		    //need to draw ceiling and other walls too
+		    glBegin(GL_QUADS);
+			glVertex3f(x, y, 1.0f);
+			glVertex3f(x, y+0.1f, 1.0f);
+			glVertex3f(x+0.1f, y+0.1f, 1.0f);
+			glVertex3f(x+0.1f, y, 1.0f);
+		    glEnd();
+		    glBegin(GL_QUADS);
+			glVertex3f(1.0f, y, x);
+			glVertex3f(1.0f, y+0.1f, x);
+			glVertex3f(1.0f, y+0.1f, x+0.1f);
+			glVertex3f(1.0f, y, x+0.1f);
+		    glEnd();
+		    glBegin(GL_QUADS);
+			glVertex3f(x, 1.0f, y);
+			glVertex3f(x, 1.0f, y+0.1f);
+			glVertex3f(x+0.1f, 1.0f, y+0.1f);
+			glVertex3f(x+0.1f, 1.0f, y);
+		    glEnd();
+		}
+	    }
+	}
 	
 	
 	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
@@ -260,102 +320,6 @@ void display()
 	    glVertex3f(0.6f, -0.9f, 1.0f);
 	    glVertex3f(0.8f, -0.9f, 1.0f);
 	glEnd();
-	
-	
-
-	
-	//draw track
-	glColor4f(1.0f, 1.0f, 0.0f, 1.0f);
-	int maxSize = sizeof(track)/sizeof(track[0]);
-	for (unsigned int i=0; i < maxSize; i++) {
-	    float tWid = 0.05f; //half of the track width
-	    float rWid = 0.025f; //half of the rail width
-	    int j = (i+1)%maxSize;
-	    /*
-	    for (int k=-1; k<3; k+=2) {
-		//left track
-		glBegin(GL_QUADS);
-		    glVertex3f(track[i][0]+((tWid+rWid)*i), track[i][1]+rWid, track[i][2]);
-		    glVertex3f(track[j][0]+((tWid+rWid)*i), track[j][1]+rWid, track[j][2]);
-		    glVertex3f(track[j][0]+(tWid*i), track[j][1]+rWid, track[j][2]);
-		    glVertex3f(track[i][0]+(tWid*i), track[i][1]+rWid, track[i][2]);
-		glEnd();
-		glBegin(GL_QUADS);
-		    glVertex3f(track[i][0]+((tWid+rWid)*i), track[i][1]-rWid, track[i][2]);
-		    glVertex3f(track[j][0]+((tWid+rWid)*i), track[j][1]-rWid, track[j][2]);
-		    glVertex3f(track[j][0]+(tWid*i), track[j][1]-rWid, track[j][2]);
-		    glVertex3f(track[i][0]+(tWid*i), track[i][1]-rWid, track[i][2]);
-		glEnd();
-		glBegin(GL_QUADS);
-		    glVertex3f(track[i][0]+((tWid+rWid)*i), track[i][1]-rWid, track[i][2]);
-		    glVertex3f(track[j][0]+((tWid+rWid)*i), track[j][1]-rWid, track[j][2]);
-		    glVertex3f(track[j][0]+((tWid+rWid)*i), track[j][1]+rWid, track[j][2]);
-		    glVertex3f(track[i][0]+((tWid+rWid)*i), track[i][1]+rWid, track[i][2]);
-		glEnd();
-		glBegin(GL_QUADS);
-		    glVertex3f(track[i][0]+(tWid*i), track[i][1]-rWid, track[i][2]);
-		    glVertex3f(track[j][0]+(tWid*i), track[j][1]-rWid, track[j][2]);
-		    glVertex3f(track[j][0]+(tWid*i), track[j][1]+rWid, track[j][2]);
-		    glVertex3f(track[i][0]+(tWid*i), track[i][1]+rWid, track[i][2]);
-		glEnd();
-	    }
-	    
-	    
-	    glBegin(GL_QUADS);
-		glVertex3f(track[i][0]-0.04f, track[i][1], track[i][2]-0.05f);
-		glVertex3f(track[j][0]-0.038f, track[j][1], track[j][2]-0.05f);
-		glVertex3f(track[j][0]+0.05f, track[j][1], track[j][2]+0.04f);
-		glVertex3f(track[i][0]+0.05f, track[i][1], track[i][2]+0.038f);
-	    glEnd();
-	    
-	    glBegin(GL_QUADS);
-		glVertex3f(track[i][0]-0.05f, track[i][1], track[i][2]-0.04f);
-		glVertex3f(track[j][0]-0.05f, track[j][1], track[j][2]-0.038f);
-		glVertex3f(track[j][0]+0.038f, track[j][1], track[j][2]+0.05f);
-		glVertex3f(track[i][0]+0.04f, track[i][1], track[i][2]+0.05f);
-	    glEnd();
-	    */
-	    
-	    /*
-	    glBegin(GL_QUADS);
-		glVertex3f(track[i][0]-0.05f, track[i][1], track[i][2]);
-		glVertex3f(track[j][0]-0.05f, track[j][1], track[j][2]);
-		glVertex3f(track[j][0]+0.05f, track[j][1], track[j][2]);
-		glVertex3f(track[i][0]+0.05f, track[i][1], track[i][2]);
-	    glEnd();
-	    */
-	    
-	    
-	    /*
-	    // enable texturing
-	    glEnable(GL_TEXTURE_2D); 
-	    
-	    //prep the texture
-	    glBindTexture(GL_TEXTURE_2D, g_track);
-	    
-	    //need to calculate track length
-	    float dx = track[i][0] - track[j][0];
-	    float dy = track[i][1] - track[j][1];
-	    float dz = track[i][2] - track[j][2];
-	    float dist = sqrt((dx*dx)+(dy*dy)+(dz*dz));
-	    dist = dist/10.0f;
-	    // specify texture coordinates
-	    glBegin (GL_QUADS);
-		glTexCoord2f (0.0f,0.0f); // lower left corner
-		glVertex3f(track[i][0]-0.05f, track[i][1], track[i][2]);
-		glTexCoord2f (1.0f, 0.0f); // lower right corner
-		glVertex3f(track[i][0]+0.05f, track[i][1], track[i][2]);
-		glTexCoord2f (1.0f, 1.0f*dist); // upper right corner
-		glVertex3f(track[j][0]+0.05f, track[j][1], track[j][2]);
-		glTexCoord2f (0.0f, 1.0f*dist); // upper left corner
-		glVertex3f(track[j][0]-0.05f, track[j][1], track[j][2]);
-	    glEnd ();
-
-	    glDisable(GL_TEXTURE_2D); 
-	    */
-	}
-	
-	
 	
 	glColor4f(1.0f, 1.0f, 0.0f, 1.0f);
 	
@@ -378,8 +342,6 @@ void display()
 		glVertex3f(pos[i][0], pos[i][1]-0.02f, pos[i][2]+0.02f);
 	    glEnd();
 	}
-	
-	
 	
 	glEnable(GL_LIGHTING);
 
@@ -445,7 +407,7 @@ void reshape(int w, int h)
     glViewport(0, 0, w, h); 
     glMatrixMode(GL_PROJECTION); 
     glLoadIdentity();
-    gluPerspective(40.0, 1.0f, 1.0, 5.0);
+    gluPerspective(40.0, 1.0f, 0.1, 5.0);
 
     glutPostRedisplay();
 }
